2c4c05a30ca68631bef27a8927ef614df1031503
Allessandra Ehab <allessandra.ehab@gmail.com>
2014-07-07 13:26:30 +0200

    updated gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  resources :all_users
-
-  resources :users
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
