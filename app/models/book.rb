cf843dce21f5c5bc9eae1f7ffb42b3a12685bb7a
Allessandra Ehab <allessandra.ehab@gmail.com>
2014-07-07 15:25:22 +0200

    last1

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,8 @@
 class Book < ActiveRecord::Base
   validates :name, :presence => true
+  validates :library_id, :presence => true
+  va1lidates :author_id, :presence => true
+
   belongs_to :library
   belongs_to :author
 end
