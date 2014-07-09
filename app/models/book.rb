9cba956f694c1b0123d06488494554e97485b85d
Allessandra Ehab <allessandra.ehab@gmail.com>
2014-07-09 14:26:02 +0200

    changes

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ActiveRecord::Base
   validates :name, :presence => true
   validates :library_id, :presence => true
-  va1lidates :author_id, :presence => true
+  validates :author_id, :presence => true
 
   belongs_to :library
   belongs_to :author
