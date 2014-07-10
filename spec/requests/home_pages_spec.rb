b00d05a06e030633d1f3bdbb9f4a52b48d726357
Allessandra Ehab <allessandra.ehab@gmail.com>
2014-07-10 12:01:14 +0200

    ll

--- spec/requests/home_pages_spec.rb
+++ spec/requests/home_pages_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 describe "Home pages" do
   describe "Home page" do
-  it "should have the content 'Our Demo App'" do
-    visit '/home_page/home'
-    page.should have_content('Our Demo App')
+    it "should have the content 'Our Demo App'" do
+      visit '/home_page/home'
+      page.should have_content('Our Demo App')
+    end
   end
-end
 end
