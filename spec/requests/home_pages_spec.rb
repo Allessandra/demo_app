ca9d4d91e3185a7109e1280774b7aea7507b8684
Allessandra Ehab <allessandra.ehab@gmail.com>
2014-07-10 12:06:13 +0200

    mm

--- spec/requests/home_pages_spec.rb
+++ spec/requests/home_pages_spec.rb
@@ -4,6 +4,7 @@ describe "Home pages" do
   describe "Home page" do
     it "should have the content 'Our Demo App'" do
       visit '/home_page/home'
+      puts page.body
       page.should have_content('Our Demo App')
     end
   end
