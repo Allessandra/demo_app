b122b79dc77af2cbf6cec1977f05bb5e9c26f0b9
Allessandra Ehab <allessandra.ehab@gmail.com>
2014-07-10 12:43:16 +0200

    test

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'rspec-rails', '~> 3.0.0'
   gem "factory_girl_rails", "~> 4.0"
   gem 'capybara'
   gem 'simplecov', '~> 0.7.1'
+  gem 'faker'
 end
 
 group :production do
--- spec/requests/home_pages_spec.rb
+++ spec/requests/home_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "Home pages" do
+describe "App pages" do
   describe "Home page" do
     it "should have the content 'Our Demo App'" do
       visit '/home_page/home'
@@ -8,4 +8,29 @@ describe "Home pages" do
       page.should have_content('Our Demo App')
     end
   end
+  
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/home_page/about'
+      puts page.body
+      page.should have_content('About Us')
+    end
+  end
+  
+  describe "Location page" do
+    it "should have the content 'Location'" do
+      visit '/home_page/location'
+      puts page.body
+      page.should have_content('Location')
+    end
+  end
+  
+  describe "Contact page" do
+    it "should have the content 'Contact Us'" do
+      visit '/home_page/contact'
+      puts page.body
+      page.should have_content('Contact Us')
+    end
+  end
+  
 end
