6d07ca10ca1f570c61160f7a606e2bf102f1ad36
Allessandra Ehab <allessandra.ehab@gmail.com>
2014-07-13 14:33:27 +0200

    test

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -4,11 +4,11 @@ RSpec.describe "Users", :type => :request do
   describe "Sign up page" do
   it "should have the h1 'Sign Up'" do
     visit signup_path
-    page.should have_content("Sign Up")
+    page.should have_content("Sign up")
   end
-   it "should have the title 'Sign Up'" do
+   it "should have the title 'Sign up'" do
     visit signup_path
-    expect(page).to have_title('Demo | Sign Up' )
+    expect(page).to have_title('Demo | Sign up' )
   end
   end
 end
