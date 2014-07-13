45d56eb3d9290601350bceada85575d5ae744281
Allessandra Ehab <allessandra.ehab@gmail.com>
2014-07-13 14:29:04 +0200

    test

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe "Users", :type => :request do
   describe "Home page" do
   it "should have the h1 'Sign Up'" do
-    visit root_path
+    visit signup_path
     page.should have_content("Sign Up")
   end
    it "should have the title 'Sign Up'" do
-    visit root_path
+    visit signup_path
     expect(page).to have_title('Demo | Sign Up' )
   end
   end
