cdc25ff8414594c45497db9fd6989c3079b75517
Allessandra Ehab <allessandra.ehab@gmail.com>
2014-07-13 13:56:30 +0200

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 
 
 Rails.application.routes.draw do
-  get 'blogs/home'
-  get 'blogs/about'
-  get 'users/new'
+  root :to => 'blogs#home'
+  get '/about' => 'blogs#about'
+  get '/signup' => 'users#new'
+  
+ 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, :type => :controller do
-
+describe "Sign Up page" do
+  it "should have the h1 'Sign Up'" do
+    visit new_path
+    page.should have_content("Sign Up")
+  end
+   it "should have the h1 'Sign Up'" do
+    visit signup_path
+    expect(page).to have_title('Demo App | Sign Up' )
+  end
+end
 end
