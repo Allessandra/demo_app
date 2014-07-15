44f2499ecdb60a0390d2263a22c30bd915041bde
Allessandra Ehab <allessandra.ehab@gmail.com>
2014-07-15 12:53:04 +0200

    ex5

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
 has_secure_password
 before_save{|user| user.email=user.email.downcase}
+before_validation do |user| 
+  o = [('a'..'z'), ('A'..'Z')].map { |i| i.to_a }.flatten 
+  string = (0...4).map { o[rand(o.length)] }.join
+  user.name=string if user.name.blank?
+end
 
 validates :name, presence: true, length:{maximum: 40}
 VAILD_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -9,4 +14,13 @@ format: { with: VAILD_EMAIL_REGEX},
 uniqueness: {case_sensitive: false}
 validates :password, presence: true ,length:{minimum:6}
 validates :password_confirmation, presence:true
+
+def self.name_longer_than_eight
+  #u_l = []
+  #User.all.each{|u| u_l.push(u) if u.name.length >= 8}
+  #puts u_l.inspect
+  
+  User.where("length(name) > 8")
+end
+
 end
